967a9e043de1934200b287f869197c9fa778097d
Maxime <maxime.miroff@icloud.com>
2024-01-02 18:01:36 +0100

    BotInfo and UserInfo Commands

---
+++ Commands/botinfo.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const {EmbedBuilder} = require("discord.js");
+
+module.exports = {
+
+    name: "botinfo",
+    description: "Affiche la latence et des informations sur le bot.",
+    permission: "Aucune",
+    category:"\\📋 • Informations :",
+    dm: true,
+
+
+    async run(bot, message, args) {
+        let webLatency = bot.ws.ping
+        const startTime = Date.now();
+        const msg = await message.reply(`Calcul en cours...`);
+        const endTime = Date.now();
+
+        const infoEmbed = new EmbedBuilder()
+            .setTitle("\\📡 Statistiques du Bot")
+            .setColor("#0099ff")
+            .addFields({name:"Latence du WebSocket", value:`\`${webLatency} ms\``, inline:true},
+                             {name:"Temps de Réponse du Serveur", value:`\`${endTime - startTime} ms\``, inline:true})
+            .addFields({name:`Serveurs et Utilisateurs du Bot :`,value:`\\📟 Serveurs : \`${bot.guilds.cache.size}\`\n\\👥 Utilisateurs : \`${bot.users.cache.size}\` `})
+
+        await msg.edit({embeds: [infoEmbed] });
+    },
+};
---
+++ Commands/userinfo.js
@@ -0,0 +1,73 @@
+const Discord = require("discord.js")
+const {EmbedBuilder} = require("discord.js");
+
+module.exports = {
+    name: "userinfo",
+    description: "Récupère des informations sur un utilisateur du serveur.",
+    permission: Discord.PermissionFlagsBits.BanMembers,
+    dm: false,
+    category:"\\📋 • Informations :",
+    options: [
+        {
+            type:"user",
+            name:"membre",
+            description:"utilisateur dont vous voulez les informations",
+            required: true,
+            autocomplete: false,
+        }
+    ],
+
+    async run(bot, message, args) {
+
+        let errorMessage = '';
+
+
+        let user = args.getUser("membre");
+        let userok = await message.guild.members.fetch(user.id).catch(() => null);
+        if (!user) {
+            errorMessage = "Le membre à mute n'a pas été trouvé.\nVérifiez les paramètres entrés et réessayez.";
+        }
+
+        if (errorMessage !== '') {
+            const occuredError = new EmbedBuilder()
+                .setColor(0xFFC600)
+                .setTitle(`\\⚠️ Une erreur est survenue.`)
+                .addFields({ name: '‎', value: `\`\`\`diff\n- Détails de l'erreur : \n\n${errorMessage}\`\`\`` })
+                .setTimestamp()
+                .setFooter({ text: 'DeltaSierra © 2024', iconURL: bot.user.displayAvatarURL() });
+
+            return message.reply({ embeds: [occuredError] });
+        }
+
+        let checkbot = " "; if(user.bot) checkbot = "\\✔️"; else checkbot = "\\❌"
+
+
+            const userRoles = userok.roles.cache
+            .filter(role => role.id !== message.guild.id) // Exclure le rôle @everyone
+            .map(role => role.toString())
+            .join(', ') || 'Aucun';
+
+        const userInfoEmbed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('\\👥 Informations Utilisateur')
+            .setThumbnail(userok.displayAvatarURL())
+            .setDescription(`
+            > **Nom d\'utilisateur :** ${user.tag} | ${user.toString()}
+            > **Tag :** ${user.tag}
+            > **ID :** ${user.id}
+            > **Bot : ** ${checkbot}
+            > **Statut :** ${userok.presence ? userok.presence.status : 'Indisponible'}
+            > **Compte créer le :** <t:${parseInt(user.createdTimestamp /1000)}:R>
+
+            __**Informations Relatives au Serveur**__
+            
+            > **A rejoint le serveur :** <t:${parseInt(userok.joinedTimestamp / 1000)}:R>
+            > **Rôle(s) :** ${userRoles}
+            `)
+            .setTimestamp()
+            .setFooter({ text: 'DeltaSierra © 2024', iconURL: bot.user.displayAvatarURL() });
+
+        await message.reply({ embeds: [userInfoEmbed] });
+
+    }
+}
